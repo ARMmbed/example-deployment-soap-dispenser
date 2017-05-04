fa82b522e0f06bae96702a81812748e92ec609b3
Andrea Corrado <andrea.corrado@arm.com>
2017-05-04 12:06:08 +0100

    Added extra functionaility, BETA tested Low battery indicator and reset counter into main branch

--- inc/ApplicationConfig.h
+++ inc/ApplicationConfig.h
@@ -6,12 +6,12 @@
 #define CONFIG_NVM_OFFSET           0x1700
 
 typedef struct {
-
-    uint32_t presses_left;
+    
+    uint32_t dispenses_left;
     uint32_t tx_interval;
     bool     initialized;
     bool     low_battery;
-
+    
 } ApplicationConfigData;
 
 
@@ -19,18 +19,18 @@ class ApplicationConfig {
 public:
     ApplicationConfig(mDot* dotInstance) : dot(dotInstance) {
         dot->nvmRead(CONFIG_NVM_OFFSET, &data, sizeof(ApplicationConfig));
-
+        
         if (!data.initialized) {
-            data.presses_left = 1000;
+            data.dispenses_left = 1000;
             data.tx_interval = 60;
             data.initialized = true;
         }
     }
-
-    uint32_t get_presses_left() {
-        return data.presses_left;
+    
+    uint32_t get_dispenses_left() {
+        return data.dispenses_left;
     }
-
+    
     uint32_t get_tx_interval_s() {
         return data.tx_interval;
     }
@@ -39,13 +39,13 @@ public:
         return data.low_battery;
     }
     
-    void decrease_presses_left() {
-        data.presses_left--;
+    void decrease_dispenses_left() {
+        data.dispenses_left--;
         persist();
     }
     
-    void reset_presses_left() {
-        data.presses_left = 1000;
+    void reset_dispenses_left() {
+        data.dispenses_left = 1000;
         persist();
     }
     
@@ -63,12 +63,12 @@ public:
         data.tx_interval = tx_interval_s;
         persist();
     }
-
+    
 private:
     void persist() {
         dot->nvmWrite(CONFIG_NVM_OFFSET, &data, sizeof(ApplicationConfig));
     }
-
+    
     mDot* dot;
     ApplicationConfigData data;
 };
--- source/main.cpp
+++ source/main.cpp
@@ -3,7 +3,7 @@
 #include "ApplicationConfig.h"
 
 static uint8_t network_id[] = { 0xBE, 0x7A, 0x00, 0x00, 0x00, 0x00, 0x03, 0x93 };
-static uint8_t network_key[] = { 0x48, 0x4A, 0x12, 0x7D, 0x98, 0xAF, 0x14, 0x36, 0xDB, 0xF3, 0xBC, 0x8C, 0xF6, 0x00, 0x44, 0x48};
+static uint8_t network_key[] = { 0x48, 0x4A, 0x12, 0x7D, 0x98, 0xAF, 0x14, 0x36, 0xDB, 0xF3, 0xBC, 0x8C, 0xF6, 0x00, 0x44, 0x48 };
 static uint8_t frequency_sub_band = 0;
 static bool public_network = true;
 static bool ack = 0;
@@ -26,27 +26,24 @@ ISL29011 lux(i2c);
 AnalogIn lux(XBEE_AD0);
 #endif
 
-static bool interrupt = false;
+static bool counter_interrupt = false;
+static bool reset_interrupt   = false;
+static bool low_battery_interrupt   = false;
 static uint32_t sleep_until = 0;
 
 void counter_dec() {
-    interrupt = true;
-    config->decrease_presses_left();
+    counter_interrupt = true;
+    config->decrease_dispenses_left();
 }
 
 void counter_reset() {
-    interrupt = true;
-    config->reset_presses_left();
+    reset_interrupt = true;
+    config->reset_dispenses_left();
 }
 
 void low_battery() {
-    interrupt = true;
+    low_battery_interrupt = true;
     config->alert_low_battery();
-}
-
-void stable_battery() {
-    interrupt = true;
-    config->alert_stable_battery();
 }
 
 int main() {
@@ -61,41 +58,39 @@ int main() {
     InterruptIn resetCounter(GPIO3);
     resetCounter.fall(&counter_reset);
     
-
-
     // gets disabled automatically when going to sleep and restored when waking up
     DigitalOut led(GPIO0, 1);
-
+    
     mts::MTSLog::setLogLevel(mts::MTSLog::TRACE_LEVEL);
-
+    
     dot = mDot::getInstance();
     config = new ApplicationConfig(dot);
-
+    
     // Custom event handler for automatically displaying RX data
     RadioEvent events(config);
-
+    
     // attach the custom events handler
     dot->setEvents(&events);
-
+    
     if (!dot->getStandbyFlag()) {
         logInfo("mbed-os library version: %d", MBED_LIBRARY_VERSION);
-
+        
         // If we start up (not from standby), and the interval is over 60s we'll set it back to 60s.
         // Not sure if we actually want this...
         if (config->get_tx_interval_s() > 60) {
             config->set_tx_interval_s(60);
         }
-
-        logInfo("configuration: presses_left=%d, tx_interval=%d", config->get_presses_left(), config->get_tx_interval_s());
-
+        
+        logInfo("configuration: dispenses_left=%d, tx_interval=%d", config->get_dispenses_left(), config->get_tx_interval_s());
+        
         // start from a well-known state
         logInfo("defaulting Dot configuration");
         dot->resetConfig();
         dot->resetNetworkSession();
-
+        
         // make sure library logging is turned on
         dot->setLogLevel(mts::MTSLog::INFO_LEVEL);
-
+        
         // update configuration if necessary
         if (dot->getJoinMode() != mDot::OTA) {
             logInfo("changing network join mode to OTA");
@@ -104,108 +99,108 @@ int main() {
             }
         }
         update_ota_config_id_key(network_id, network_key, frequency_sub_band, public_network, ack);
-
+        
         // configure network link checks
         // network link checks are a good alternative to requiring the gateway to ACK every packet and should allow a single gateway to handle more Dots
         // check the link every count packets
         // declare the Dot disconnected after threshold failed link checks
         // for count = 3 and threshold = 5, the Dot will be considered disconnected after 15 missed packets in a row
         update_network_link_check_config(3, 5);
-
+        
         logInfo("enabling ADR");
         if (dot->setAdr(true) != mDot::MDOT_OK) {
             logError("failed to enable ADR");
         }
-
+        
         // Start in SF_10, and then ADR will find the most applicable datarate
         logInfo("setting TX datarate to SF_10");
         if (dot->setTxDataRate(mDot::SF_10) != mDot::MDOT_OK) {
             logError("failed to set TX datarate");
         }
-
+        
         // save changes to configuration
         logInfo("saving configuration");
         if (!dot->saveConfig()) {
             logError("failed to save configuration");
         }
-
+        
         // display configuration
         display_config();
     } else {
         logInfo("restoring network session from NVM");
         dot->restoreNetworkSession();
     }
-
+    
+    logInfo("Outside the while loop...");
+    
     while (true) {
-        if (interrupt) {
-            interrupt = false;
-
+        logInfo("Inside the while loop. counter_interrupt=%d, reset_interrupt=%d, low_battery_interrupt=%d",
+                counter_interrupt, reset_interrupt, low_battery_interrupt);
+        
+        if (counter_interrupt) {
+            counter_interrupt = false;
+            
             // we only care about waking up from RTC, so go back to sleep asap
             uint32_t sleep_time = sleep_until - time(NULL);
             logInfo("Woke from interrupt, going back to sleep for %d seconds", sleep_time);
             sleep_wake_rtc_or_interrupt(sleep_time, deep_sleep);
             continue;
         }
-
+        
+        if (reset_interrupt) {
+            reset_interrupt = false;
+            
+            // do reset stuff
+            logInfo("USER Reset Dispenses to 1000(Full)");
+        }
+        
         std::vector<uint8_t> tx_data;
-
+        
         // join network if not joined
         if (!dot->getNetworkJoinStatus()) {
             join_network();
         }
-
-        uint32_t presses_left = config->get_presses_left();
-
-        tx_data.push_back((presses_left >> 8) & 0xff);
-        tx_data.push_back(presses_left & 0xff);
         
-        if (resetCounter==0) {
-            config->reset_presses_left();
-            logInfo("Replenished gel");
-        }
+        uint32_t dispenses_left = config->get_dispenses_left();
         
-        //To clear the lowBat status catching high interrupt will not work, simply check status of the GPIO on runtime and resets it here.
-        if (lowBat==1) {
-            stable_battery();
+        tx_data.push_back((dispenses_left >> 8) & 0xff);
+        tx_data.push_back(dispenses_left & 0xff);
+        
+        //As this is interrupt driven above, simply get status of battery, will only push data if low battery
+        
+        if ((lowBat.read() == 1)&&(config->get_battery_status())) {
+            config->alert_stable_battery();
             tx_data.push_back(0x00);
             logInfo("Battery voltage healthy");
         }
         
-//        else {
-//            low_battery();
-//            tx_data.push_back(0x01);
-//            logInfo("ALERT! low battery");
-//        }
-
-        
-        //As this is interrupt drive above, simply get status of battery, will only push data is low battery
-        if (config->get_battery_status()) {
+        if (lowBat.read() == 0) {
+            config->alert_low_battery();
             tx_data.push_back(0x01);
-            logInfo("ALERT! low battery");
-            
+            logInfo("ALERT! Low battery");
         }
         
-        logInfo("Sending presses %d", presses_left);
-
+        
+        
+        logInfo("Sending dispenses left %d", dispenses_left);
+        
         send_data(tx_data);
         
-        
-
         // if going into deepsleep mode, save the session so we don't need to join again after waking up
         // not necessary if going into sleep mode since RAM is retained
         if (deep_sleep) {
             logInfo("saving network session to NVM");
             dot->saveNetworkSession();
         }
-
+        
         uint32_t sleep_time = calculate_actual_sleep_time(config->get_tx_interval_s());
         sleep_until = time(NULL) + sleep_time;
-
+        
         // ONLY ONE of the three functions below should be uncommented depending on the desired wakeup method
         //sleep_wake_rtc_only(deep_sleep);
         //sleep_wake_interrupt_only(deep_sleep);
         sleep_wake_rtc_or_interrupt(sleep_time, deep_sleep);
     }
-
+    
     return 0;
 }
